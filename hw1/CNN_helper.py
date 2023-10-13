bbde9edb658f2b77d8158f61b128962cd773dffb
Baolin Li <baolin.li1994@gmail.com>
2023-10-13 18:06:19 -0400

    HW1 models working

--- hw1/CNN_helper.py
+++ hw1/CNN_helper.py
@@ -7,9 +7,9 @@ class MNIST_Classifier(nn.Module):
     def __init__(self):
         super(MNIST_Classifier, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, 5, padding=2) # 28 -> 28
-        self.pool = nn.MaxPool2d(2, 2)
+        self.pool = nn.MaxPool2d(2, 2) # 28 -> 14
         self.conv2 = nn.Conv2d(32, 64, 3, padding=1) # 14 -> 14
-        self.fc1 = nn.Linear(64 * 7 * 7, 128)
+        self.fc1 = nn.Linear(64 * 7 * 7, 128) # each channel is 7x7 after pooling
         self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
@@ -24,19 +24,34 @@ class MNIST_Classifier(nn.Module):
 
 # define the CNN architecture
 class CIFAR10_Classifier(nn.Module):
-    def __init__(self):
-        super(MNIST_Classifier, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(32, 64, 5)
-        self.fc1 = nn.Linear(64 * 4 * 4, 1024)
+    def __init__(self, dropout_prob=0.4):
+        super(CIFAR10_Classifier, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=1) # 32 -> 32
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1) # 32 -> 32
+        self.bn2 = nn.BatchNorm2d(64)
+        self.pool1 = nn.MaxPool2d(2, 2) # 32 -> 16
+        self.conv3 = nn.Conv2d(64, 128, 3, padding=1) # 16 -> 16
+        self.bn3 = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(128, 256, 3, padding=1) # 16 -> 16
+        self.bn4 = nn.BatchNorm2d(256)
+        self.pool2 = nn.MaxPool2d(2, 2) # 16 -> 8
+        self.fc1 = nn.Linear(256 * 8 * 8, 1024) # each channel is 8x8 after pooling
+        self.bn5 = nn.BatchNorm1d(1024)
         self.fc2 = nn.Linear(1024, 10)
+        self.dropout = nn.Dropout(dropout_prob)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        print(x.shape)
-        x = x.view(-1, 64 * 4 * 4)
-        x = F.relu(self.fc1(x))
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool1(x)
+        x = self.dropout(x)
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = self.pool2(x)
+        x = self.dropout(x)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.bn5(self.fc1(x)))
+        x = self.dropout(x)
         x = self.fc2(x)
-        return x    
+        return x
--- hw1/CNNclassify.py
+++ hw1/CNNclassify.py
@@ -17,9 +17,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 from CNN_helper import MNIST_Classifier, CIFAR10_Classifier
 import argparse
-
+from torch.utils.tensorboard import SummaryWriter
+from datetime import datetime
+from pathlib import Path
 
 def trainCNN(args):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(f"Training using {device}")
+
     # load the dataset
     if args.mnist:
         transform = transforms.Compose([
@@ -48,82 +53,105 @@ def trainCNN(args):
         print('Error: unrecognized dataset name')
         return
 
+    current_time = datetime.now().strftime('%Y%m%d-%H%M%S')
+    log_dir = f"./runs/{type(model).__name__}/{current_time}"
+    writer = SummaryWriter(log_dir=log_dir)
 
+    model.to(device)
     # define the loss function and optimizer
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(net.parameters(), lr=args.lr, momentum=0.9)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
     # train the network
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,
-                                              shuffle=True, num_workers=2)
+                                              shuffle=True, num_workers=4)
     testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size,
-                                             shuffle=False, num_workers=2)
+                                             shuffle=False, num_workers=4)
+
+    print(f"{'Epoch':<6} | {'Train Loss':<10} | {'Train Acc%':<10} | {'Test Loss':<10} | {'Test Acc%':<10}")
+    print('-' * 60)  # print a separator line
+
+    best_test_accuracy = 0.0
+    Path.mkdir(Path('./model'), exist_ok=True)
 
     for epoch in range(args.epochs):  # loop over the dataset multiple times
-        running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
+        model.train()
+        train_loss = 0.0
+        correct_train_predictions = 0
+        total_train_predictions = 0
 
-            # get the inputs
-            inputs, labels = data
-
+        for inputs, labels in trainloader:
+            inputs, labels = inputs.to(device), labels.to(device)
             # zero the parameter gradients
             optimizer.zero_grad()
 
             # forward + backward + optimize
-            # print(inputs.shape)
-            outputs = net(inputs)
+            outputs = model(inputs)
             # print(outputs.shape)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
+            # evaluate 
+            train_loss += loss.item()
 
+            _, predicted = outputs.max(1)
+            total_train_predictions += labels.size(0)
+            correct_train_predictions += (predicted == labels).sum().item()
+
+        avg_train_loss = train_loss / len(trainloader)
+        train_accuracy = (correct_train_predictions / total_train_predictions) * 100
+        writer.add_scalar('Loss/train', avg_train_loss, epoch)
+        writer.add_scalar('Accuracy/train', train_accuracy, epoch)
+
+        model.eval()
+        test_loss = 0.0
+        correct_test_predictions = 0
+        total_test_predictions = 0
+
+        with torch.no_grad():
+            for inputs, labels in testloader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model(inputs)
+                test_loss += criterion(outputs, labels).item()
+
+                _, predicted = outputs.max(1)
+                total_test_predictions += labels.size(0)
+                correct_test_predictions += (predicted == labels).sum().item()
+
+        avg_test_loss = test_loss / len(testloader)
+        test_accuracy = (correct_test_predictions / total_test_predictions) * 100
+        writer.add_scalar('Loss/test', avg_test_loss, epoch)
+        writer.add_scalar('Accuracy/test', test_accuracy, epoch)
+        if test_accuracy > best_test_accuracy:
+            best_test_accuracy = test_accuracy
+            torch.save(model.state_dict(), f'./model/{type(model).__name__}.pth')
+
+        if epoch % args.print_freq == 0:
             # print statistics
-            running_loss += loss.item()
-            # print(running_loss)
-            if i % 2000 == 1999:
-                print('[%d, %5d] loss: %.3f' %
-                      (epoch, i, running_loss))
-                running_loss = 0.0
+            print(f"{epoch+1:<6} | {avg_train_loss:<10.4f} | {train_accuracy:<10.2f} | {avg_test_loss:<10.4f} | {test_accuracy:<10.2f}")
 
+    writer.close()
     print('Finished Training')
 
-    # save the trained model
-    PATH = './' + dataset_name + '_net.pth'
-    torch.save(net.state_dict(), PATH)
+    # # save the trained model
+    # PATH = './' + dataset_name + '_net.pth'
+    # torch.save(net.state_dict(), PATH)
 
-    # test the network on the test data
-    dataiter = iter(testloader)
-    images, labels = dataiter.next()
-    # print images
-    # imshow(torchvision.utils.make_grid(images))
-    # plt.show()
-    print('GroundTruth: ', ' '.join('%5s' % labels[j].numpy() for j in range(4)))
+    # # test the network on the test data
+    # dataiter = iter(testloader)
+    # images, labels = dataiter.next()
+    # # print images
+    # # imshow(torchvision.utils.make_grid(images))
+    # # plt.show()
+    # print('GroundTruth: ', ' '.join('%5s' % labels[j].numpy() for j in range(4)))
 
-    net = Classifier()
-    net.load_state_dict(torch.load(PATH))
-    outputs = net(images)
-    _, predicted = torch.max(outputs, 1)
-    print('Predicted: ', ' '.join('%5s' % predicted[j].numpy()
-                                  for j in range(4)))
-    
-    # test the network on the whole test data  
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for data in testloader:
-            images, labels = data
-            # print(images.shape)
-            outputs = net(images)
-            # print(outputs.shape)
-            _, predicted = torch.max(outputs.data, 1)
-            # print(predicted.shape)
-            total += labels.size(0)
-            # print(total)
-            correct += (predicted == labels).sum().item()
-            # print(correct)
-    print('Accuracy of the network on the 10000 test images: %d %%' % (
-        100 * correct / total))
-    
+    # net = Classifier()
+    # net.load_state_dict(torch.load(PATH))
+    # outputs = net(images)
+    # _, predicted = torch.max(outputs, 1)
+    # print('Predicted: ', ' '.join('%5s' % predicted[j].numpy()
+    #                               for j in range(4)))
+        
 
 def main():
     parser = argparse.ArgumentParser(description='CNN classification')
@@ -131,11 +159,12 @@ def main():
     subparsers = parser.add_subparsers(dest="command", help="Sub-command to run.")
 
     train_parser = subparsers.add_parser("train", help="Train the model.")
-    train_parser.add_argument("--epochs", type=int, default=200, help="Number of epochs for training.")
+    train_parser.add_argument("--epochs", type=int, default=10, help="Number of epochs for training.")
     train_parser.add_argument("--lr", type=float, default=0.001, help="Learning rate for training.")
     train_parser.add_argument("--mnist", action="store_true", help="Train on MNIST dataset.")
     train_parser.add_argument("--cifar", action="store_true", help="Train on CIFAR dataset.")
-    train_parser.add_argument("--batch_size", type=int, default=32, help="Batch size for training.")
+    train_parser.add_argument("--batch_size", type=int, default=128, help="Batch size for training.")
+    train_parser.add_argument("--print_freq", type=int, default=1, help="Print frequency")
 
     args = parser.parse_args()
 
